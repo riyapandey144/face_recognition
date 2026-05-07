53890c30a63f29c7816fc3f7fec483e3610570a1
Gaurav Pandey <pamdeygaurav911@gmail.com>
2026-05-07 15:33:25 +0530

    Fix Vercel serverless function configuration

--- api/index.py
+++ api/index.py
@@ -14,7 +14,6 @@ sys.path.insert(0, str(DJANGO_DIR))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 app = get_wsgi_application()
-application = app
 
 if os.environ.get("VERCEL") and os.environ.get("DJANGO_AUTO_MIGRATE", "1") == "1":
     from django.core.management import call_command
